179febe97a9f911d6712b95b524219618b57cd3f
nazywam <nazywam@gmail.com>
2015-10-23 20:28:23 +0200

    Finished actor class

--- source/Actor.hx
+++ source/Actor.hx
@@ -1,13 +1,13 @@
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
 
+class Actor extends FlxSprite {
 
-clas Actor extends FlxSprite {
-
-	public var gridPos
+	public var gridPos:FlxPoint;
 
 	override public function new(gridX:Float, gridY:Float){
-		super();
-
-		
+		super(100, 100);
+		gridPos = new FlxPoint(gridX, gridY);
 
 	}
 }
